a967d5f15040a657f7af1a194bf3c54e9fc15ac0
Ahmedbenkhalifa <ahmedbenkhalifa1997@gmail.com>
2022-08-05 13:30:35 +0100

    add footer component

---
+++ src/assets/Icons/FacebookIcon.js
@@ -0,0 +1,36 @@
+import * as React from "react";
+
+const FacebookIcon = (props) => (
+  <svg
+    viewBox="0 0 78 78"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <circle
+      cx={38.928}
+      cy={38.928}
+      r={38.014}
+      fill="#2A2E40"
+      stroke="#fff"
+      strokeWidth={1.829}
+    />
+    <g clipPath="url(#a)">
+      <path
+        d="m49.945 42.162 1.27-8.273h-7.939V28.52c0-2.262 1.109-4.47 4.664-4.47h3.609v-7.042s-3.274-.559-6.406-.559c-6.537 0-10.81 3.963-10.81 11.136v6.305h-7.267v8.273h7.267v20h8.943v-20l6.669-.001Z"
+        fill="#FFF"
+      />
+    </g>
+    <defs>
+      <clipPath id="a">
+        <path
+          fill="#fff"
+          transform="translate(16.453 16.45)"
+          d="M0 0h45.714v45.714H0z"
+        />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+export default FacebookIcon;
---
+++ src/components/containers/FooterContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Box } from "@mui/material";
+
+const FooterContainer = ({ children }) => {
+  return <Box sx={{ my: 4 }}> {children}</Box>;
+};
+
+export default FooterContainer;
